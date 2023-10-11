25dddbfdf284844097a28fc4f8a391fd1e9e5fd5
nayoseb <nayoseb1018@gmail.com>
2023-10-11 10:25:19 +0900

    백준2751수정렬하기

---
+++ java/org/example/백준2751수정렬하기/Main.java
@@ -0,0 +1,28 @@
+package 백준2751수정렬하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+        bufferedReader.close();
+
+        Arrays.sort(arr);
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            stringBuilder.append(arr[i]);
+            stringBuilder.append("\n");
+        }
+        System.out.println(stringBuilder);
+    }
+}
