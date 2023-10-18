4322498da4be006714bce96a14341526bfc16480
nayoseb <nayoseb1018@gmail.com>
2023-10-18 13:29:42 +0900

    백준2154카드2

---
+++ java/org/example/class2/백준2154카드2/Main.java
@@ -0,0 +1,27 @@
+package class2.백준2154카드2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i < N+1; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size()>1) {
+            queue.poll();
+            int M = queue.poll();
+            queue.add(M);
+        }
+        int result = queue.poll();
+        System.out.println(result);
+    }
+}
