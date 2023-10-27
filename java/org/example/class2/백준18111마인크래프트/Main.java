4032b473793f637238fbfcbcd2a4939a5a4a1d6e
nayoseb <nayoseb1018@gmail.com>
2023-10-27 09:33:57 +0900

    백준18111마인크래프트

---
+++ java/org/example/class2/백준18111마인크래프트/Main.java
@@ -0,0 +1,62 @@
+package class2.백준18111마인크래프트;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][M];
+        int min = 256;
+        int max = 0;
+
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                if(min > arr[i][j]) min = arr[i][j];
+                if(max < arr[i][j]) max = arr[i][j];
+            }
+        }
+
+
+        int time = Integer.MAX_VALUE;
+        int height = 0;
+        for(int i = min; i <= max; i++) {
+            int count = 0;
+            int block = B;
+            for(int j = 0; j < N; j++) {
+                for(int k = 0; k < M; k++) {
+                    if(i < arr[j][k]) {
+                        count += ((arr[j][k] - i) * 2);
+                        block += (arr[j][k] - i);
+                    }else {
+                        count += (i - arr[j][k]);
+                        block -= (i - arr[j][k]);
+                    }
+                }
+            }
+            if(block < 0) break;
+
+            if(time >= count) {
+                time = count;
+                height = i;
+            }
+        }
+        System.out.println(time + " " + height);
+    }
+
+}
+
+
+
+
+
+
+
