f1f644d860837394155a90a6472f00fb54757559
nayoseb <nayoseb1018@gmail.com>
2023-11-10 09:05:45 +0900

    백준11726_2xN_타일링2

---
+++ java/org/example/class3/백준11726_2xN_타일링2/Main.java
@@ -0,0 +1,24 @@
+package class3.백준11726_2xN_타일링2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args )throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine());
+
+        long[] dp = new long[1001];
+        dp[1] = 1L;
+        dp[2] = 3L;
+
+        for (int i = 3; i < n+1; i++) {
+            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+
+    }
+}
